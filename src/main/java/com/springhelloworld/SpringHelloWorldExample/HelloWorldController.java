7ee8442e54cb4d41e9fd83f031bc6d7f3b1e69a9
bhdrerdem <bahadir.erdem@hotmail.com>
2022-04-27 16:56:21 +0300

    initial commit

---
+++ src/main/java/com/springhelloworld/SpringHelloWorldExample/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.springhelloworld.SpringHelloWorldExample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    String helloWorld(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/springhelloworld/SpringHelloWorldExample/SpringHelloWorldExampleApplication.java
@@ -0,0 +1,15 @@
+package com.springhelloworld.SpringHelloWorldExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class SpringHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldExampleApplication.class, args);
+	}
+
+}
