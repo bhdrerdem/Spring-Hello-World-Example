e7d64b68a8ae0fb9827323cbf57e9a4f115baa32
bhdrerdem <bahadir.erdem@hotmail.com>
2022-04-27 16:56:21 +0300

    initial commit

---
+++ src/test/java/com/springhelloworld/SpringHelloWorldExample/SpringHelloWorldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhelloworld.SpringHelloWorldExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
